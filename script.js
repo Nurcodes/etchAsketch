76bc0a2dacac8005c87f3ebafc329481ce1f177f
Abdifatah Nur <deenynur21@gmail.com>
2023-02-08 14:59:37 -0800

    Made some functionality and ui updates

---
+++ script.js
@@ -0,0 +1,51 @@
+const container = document.querySelector(".container");
+const sidebar = document.querySelector(".sidebar");
+const btn = document.createElement("button");
+// btn.textContent = "Click me";
+// sidebar.insertAdjacentElement("beforeend", btn);
+const hex = ["a", "b", "c", "d", "e", "f", 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function makeGrid(size) {
+  let div;
+  const grid = size ** 2;
+  for (let i = 0; i < grid; i++) {
+    div = `<div class="cell" data-cell="${i + 1}"></div>`;
+    container.insertAdjacentHTML("beforeend", div);
+  }
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+}
+
+function removeColorHover(e) {
+  if (
+    !e.target.classList.contains("cell") ||
+    e.target.style.background == "black"
+  )
+    return;
+  e.target.style.backgroundColor = "black";
+}
+
+function randomColorHover(e) {
+  if (!e.target.classList.contains("cell")) return;
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * hex.length)];
+  }
+
+  e.target.style.backgroundColor = color;
+}
+
+//Event listeners
+btn.addEventListener("click", function (e) {
+  const size = prompt("Enter size of grid");
+
+  if (size > 100 || size <= 0) {
+    alert("Grid is invalid Enter a number between 1 and 100!!");
+  } else {
+    container.innerHTML = "";
+    makeGrid(size);
+  }
+});
+
+container.addEventListener("mouseover", randomColorHover);
+// container.addEventListener("mouseout", removeColorHover);
